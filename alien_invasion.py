e225b44ba5c9ee03ac7cc3db53dcdae10e35057d
wldasf <waleed.a.assaf@gmail.com>
2024-02-05 22:11:51 +0300

    fix syntax error

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,11 +21,11 @@ class AlienInvasion:
     def run_game(self):
         while True:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT():
+                if event.type == pygame.QUIT:
                     sys.exit()
 
             pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
-    ai .run_game()
+    ai.run_game()
