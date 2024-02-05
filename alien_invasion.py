e4873bbb7251ca731d574c423dd63a1832207418
wldasf <waleed.a.assaf@gmail.com>
2024-02-05 20:58:33 +0300

    baseline

---
+++ alien_invasion.py
@@ -0,0 +1,10 @@
+import sys
+import pygame
+
+"""
+- Blue Sky: Make a Pygame window with a blue background.
+- Game Character: Find a bitmap image of a game character you like or
+convert an image to a bitmap. Make a class that draws the character at the
+center of the screen, then match the background color of the image to the background color of the screen or vice versa.
+
+"""
