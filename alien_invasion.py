2cbb32aa4e9ca25911fe1270b6470714bf99ddb5
wldasf <waleed.a.assaf@gmail.com>
2024-04-15 11:32:12 +0300

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,13 +19,18 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.bg_color = (0, 0, 255)
+
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.bg_color)            
+
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
