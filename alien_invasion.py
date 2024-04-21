021921378fdaf71a41cda072fce5bbf8d6654fb0
wldasf <waleed.a.assaf@gmail.com>
2024-04-21 07:19:37 +0300

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,14 +4,6 @@ import pygame
 from settings import Settings
 from ship import Ship
 
-"""
-# TODO
-- Game Character: Find a bitmap image of a game character you like or
-convert an image to a bitmap. Make a class that draws the character at the
-center of the screen, then match the background color of the image to the background color of the screen or vice versa.
-
-"""
-
 class AlienInvasion:
 
     def __init__(self):
