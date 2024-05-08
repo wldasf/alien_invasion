c3053c2619dd26832873d78b3389bd04a545681f
wldasf <waleed.a.assaf@gmail.com>
2024-05-08 16:53:36 +0300

    change ship speed to 1.5 and add continuous left and right movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -30,7 +31,15 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)            
--- settings.py
+++ settings.py
@@ -4,3 +4,5 @@ class Settings:
         self.screen_width = 1200 
         self.screen_height = 800
         self.bg_color = (0, 0, 0)
+
+        self.ship_speed = 1.5
