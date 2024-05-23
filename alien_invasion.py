0c49be0d2a8f7546ef4acc099bda678ec7a225a0
wldasf <waleed.a.assaf@gmail>
2024-05-23 14:43:21 +0300

    add up and down movement and prevent crossing window border

--- alien_invasion.py
+++ alien_invasion.py
@@ -48,6 +48,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_top = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_bottom = True
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -56,6 +60,10 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_top = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_bottom = False
 
 
 
--- ship.py
+++ ship.py
@@ -10,20 +10,28 @@ class Ship:
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.center = self.screen_rect.center
 
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         self.moving_right = False
         self.moving_left = False
+        self.moving_top = False
+        self.moving_bottom = False
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        if self.moving_bottom and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        if self.moving_top and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
 
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
