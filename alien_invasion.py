c8abbafe90e3e037a66ce0926252ea54035d11f4
wldasf <waleed.a.assaf@gmail.com>
2024-08-03 20:21:48 +0300

    add vertical fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,18 +79,22 @@ class AlienInvasion:
 
     def _create_fleet(self):
         alien = Alien(self)
-        # self.aliens.add(alien)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self._create_alien(current_x)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
 
-    def _create_alien(self, x_position):
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
             new_alien = Alien(self)
             new_alien.x = x_position
             new_alien.rect.x = x_position
+            new_alien.rect.y = y_position
             self.aliens.add(new_alien)
 
     def _update_screen(self):
