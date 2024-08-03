cf4c9b38b7e76b49edddb2935bdafedc16d8abfa
wldasf <waleed.a.assaf@gmail.com>
2024-08-03 20:04:24 +0300

    add screen alien fleet

--- alien.py
+++ alien.py
@@ -6,7 +6,7 @@ class Alien(Sprite):
         super().__init__()
         self.screen = ai_game.screen
 
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('images/alien.png')
         self.rect = self.image.get_rect()
 
         self.rect.x = self.rect.width
--- alien_invasion.py
+++ alien_invasion.py
@@ -79,7 +79,16 @@ class AlienInvasion:
 
     def _create_fleet(self):
         alien = Alien(self)
-        self.aliens.add(alien)
+        # self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)            
