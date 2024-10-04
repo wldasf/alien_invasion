9bae6787651c9233856c63b445b8192c62703cda
wldasf <waleed.a.assaf@gmail.com>
2024-10-04 12:21:56 +0300

    increase ship speed

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (0, 0, 0)
 
-        self.ship_speed = 1.5
+        self.ship_speed = 5.0
 
         self.bullet_speed = 2.0
         self.bullet_width = 3
