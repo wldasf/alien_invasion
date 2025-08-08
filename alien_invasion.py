468b307ff9567d083d87cefc6405f7f1ee3908b4
wldasf <waleed.a.assaf@gmail.com>
2025-08-08 19:14:45 +0300

    ability to shoot aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -78,6 +78,8 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def _update_aliens(self):
         self._check_fleet_edges()
         self.aliens.update()
